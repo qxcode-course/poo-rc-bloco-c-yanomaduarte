d74e89359a009f9f437ab72ba513f44771cd2c1d
Yan Notebook <yanduarte13@gmail.com>
2025-11-14 20:20:37 -0300

    fix: tests error

--- myrep/poo/cinema/py/draft.py
+++ myrep/poo/cinema/py/draft.py
@@ -1,4 +1,4 @@
-class Cliente:
+class Client:
     def __init__(self, id: str, phone: int):
         self.__id = id
         self.__phone = phone
@@ -21,11 +21,63 @@ class Cliente:
 
 class Theater:
     def __init__(self, capacity: int):
-        self.__capacity = capacity
+        self.__seats: list[Client | None] = [None] * capacity
+
+    def __verifyIndex(self, index: int):
+        if 0 <= index < len(self.__seats):
+            return True
+        return False
+
+    def __search(self, id: str) -> int:
+        for i, client in enumerate(self.__seats):
+            if client is not None and client.getId() == id:
+                return i
+        return -1
+
+    def reserve(self, id: str, phone: int, index: int) -> bool:
+        if not self.__verifyIndex(index):
+            print(f"fail: cadeira nao existe")
+            return False
+
+        if self.__seats[index] is not None:
+            print("fail: caideira ja esta ocupada")
+            return False
+
+        if self.__search(id) != -1:
+            print(f"fail: cliente {id} ja esta no cinema")
+            return False
+
+        new_client = Client(id, phone)
+        self.__seats[index] = new_client
+        return True
+
+    def cancel(self, id: str):
+        index = self.__search(id)
+        if index == -1:
+            print(f"fail: cliente nao esta no cinema")
+            return
+
+        self.__seats[index] = None
+
+    def getSeats(self) -> list[Client | None]:
+        return self.__seats
+
+    def toString(self):
+        seat_strings = []
+        for client in self.__seats:
+            if client is None:
+                seat_strings.append("-")
+            else:
+                seat_strings.append(str(client))
+
+        return "[ " + " ".join(seat_strings) + " ]"
+
+    def __str__(self) -> str:
+        return self.toString()
 
 
 def main():
-    theater = Theater()
+    theater = None
 
     while True:
         line = input()
@@ -39,3 +91,27 @@ def main():
 
         if cmd == "end":
             break
+
+        elif cmd == "init":
+            capacity = int(args[1])
+            theater = Theater(capacity)
+
+        elif cmd == "show":
+            if theater:
+                print(theater)
+
+        elif cmd == "reserve":
+            if theater:
+                id = args[1]
+                phone = int(args[2])
+                index = int(args[3])
+                theater.reserve(id, phone, index)
+
+        elif cmd == "cancel":
+            if theater:
+                id = args[1]
+                theater.cancel(id)
+
+
+if __name__ == "__main__":
+    main()
